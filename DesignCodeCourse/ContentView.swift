fd6bc15f97f239b27df3b0660c3d871acaa6bbd5
Will Wang <willwang4859@gmail.com>
2020-09-06 14:59:34 -0500

    Initial Commit

---
+++ DesignCodeCourse/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignCodeCourse
+//
+//  Created by Will Wang on 9/6/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DesignCodeCourse/DesignCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourseApp.swift
+//  DesignCodeCourse
+//
+//  Created by Will Wang on 9/6/20.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
